fc32968486866befd584bad2ba5678bef45d2f3d
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 23:56:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,12 @@
 class MusicLibraryController
 
-    def initialize(path)
+  
+  def initialize(path='./db/mp3s')
+    @path = path
+    @music_importer = MusicImporter.new(@path).tap do |importer|
+      importer.import
     end
+  end
 
 
 end
