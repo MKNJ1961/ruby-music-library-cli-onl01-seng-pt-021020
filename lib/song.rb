3934eed637ec06393f81a1ada04255794dda9445
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 13:56:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,9 +40,9 @@ class Song
     all.find{ |song| song.name == name}
   end
 
-def self.find_or_create_by_name(name)
-  find_by_name(name) || create(name)
-end
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
 
 
   def self.create(name)
