3d294d887fbf89cea0dbb3517857ee310dd8b4c4
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 14:41:49 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -8,7 +8,7 @@ class MusicImporter
   end
 
   def files
-    Dir.glob("#{(path}/*").map{ |file| foile.gsub("#{path}/", '')})
+    Dir.glob("#{(path}/*").map{ |file| file.gsub("#{path}/", '')})
 
   end
 
