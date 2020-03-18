1c782f74b2d29a8a678a446464d99539344e6a8c
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 12:35:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,51 +43,51 @@ class Song
 end
 # require'pry'
 # class Song
-# 
+#
 #   attr_accessor :name
 #   attr_reader :artist, :genre
-# 
-# 
+#
+#
 #   @@all = []
-# 
+#
 #   def initialize(name, artist=nil, genre = nil)
 #     @name = name
 #     self.artist = artist if artist
 #     self.genre = genre if genre
 #   end
-# 
+#
 #   def artist=(artist)
 #     @artist = artist
 #     artist.add_song(self)
 #   end
-# 
+#
 #   def genre=(genre)
 #     @genre = genre
 #     genre.songs.push self unless genre.songs.include? self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def self.destroy_all
 #     all.clear
 #   end
-# 
+#
 #   def save
 #     self.class.all << self
 #   end
-# 
+#
 #   def self.find_by_name(name)
 #     all.find(&:name)
 #   end
-# 
+#
 #   def self.create(name)
 #     song = new(name)
 #     song.save
 #     song
 #   end
-# 
+#
 # end
 
 
