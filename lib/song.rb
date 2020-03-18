cc753fa781b6e25922ce30399fe8219bc202dbda
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 15:29:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,6 +50,23 @@ class Song
     song
   end
 
+  def self.new_from_filename(filename)
+    parms = filename.split(" - ")
+    artist = Artist.find_or_create_by_name(parms[0])
+    song = self.new(parms[1])
+    songGenre=parms[2].slice(0..parms[2].length-5)
+    genre = Genre.find_or_create_by_name(songGenre)
+    song.artist=artist
+    song.genre=genre
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song=self.new_from_filename(filename)
+    song.save
+    song
+  end
+
 end
 
 
