7979acd47ce7ac7a81a0215cbf1fc1e59b6b1d83
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 12:37:15 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,41 +1,62 @@
+# class Genre
+# 
+#   attr_accessor :name
+#   attr_reader :songs
+# 
+#   @@all = []
+# 
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.destroy_all
+#     all.clear
+#   end
+# 
+#   def save
+#     self.class.all << self
+#   end
+# 
+#   def self.create(name )
+#     genre = new(name)
+#     genre.save
+#     genre
+#   end
+# 
+#   def artists
+#     songs.map(&:artist).uniq
+#   end
+# 
+# end
+# 
+# 
 class Genre
-
-  attr_accessor :name
-  attr_reader :songs
+  # extend Concerns::Findable
+  # include Concerns::Savable
 
   @@all = []
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
 
   def self.all
     @@all
   end
+  attr_accessor :songs, :name
 
-  def self.destroy_all
-    all.clear
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.create(name )
-    genre = new(name)
-    genre.save
-    genre
+  def initialize(name)
+    super(name)
+    self.save unless Genre.all.map{|g| g.name}.include?(name)
+    @songs = []
   end
 
   def artists
-    songs.map(&:artist).uniq
+    self.songs.map{|song| song.artist }.uniq
   end
 
 end
-
-
-
 
 
 
