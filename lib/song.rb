f0ef79642d4e529df392d1ce339ee25b758304c9
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-18 10:52:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,10 @@ class Song
     @artist = artist
   end
 
-  # def artist=(artist)
-  #   @artist = artist
-  #   artist.add_song(self)
-  # end
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
 
   def self.all
     @@all
