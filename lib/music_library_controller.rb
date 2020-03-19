17e68ea1f670b6b9b46ccadd960aded796cc35a9
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-19 01:01:15 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,6 +21,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+
+    imput = gets.strip
   end
 
 
