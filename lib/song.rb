ab36f726a4bf3c61b38218aaac9bf13f17f32611
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-19 00:41:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,13 +59,9 @@ class Song
 
   end
 
-  # def self.create_from_filename(filename)
-  #   song=self.new_from_filename(filename)
-  #   song.save
-  #   song
-  # end
-
-end
+  def self.create_from_filename(name)
+    new_from_filename(name).save
+  end
 
 
 
