03a56986d5c2e4b1b4e8be97761ef08c9d65c9e4
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-19 02:15:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -80,7 +80,7 @@ class MusicLibraryController
   # def play_song
   #   puts "Which song number would you like to play?"
   #   input = gets.strip
-  # 
+  #
   #   if song_index = input.to_i
   #     song = Song.all[song_index-1]
   #     puts "Playing #{song.name} by #{song.artist.name}"
