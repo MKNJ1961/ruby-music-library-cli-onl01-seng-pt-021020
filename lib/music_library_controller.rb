28f91930fc2ef936613dee84188690d21b15a387
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-19 02:29:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -101,21 +101,21 @@ end
 
 
 # class MusicLibraryController
-# 
+#
 #   attr_accessor :path, :music_importer
-# 
-# 
+#
+#
 #   def initialize(path='./db/mp3s')
 #     @path = path
 #     @music_importer = MusicImporter.new(@path).tap do |importer|
 #       importer.import
 #     end
 #   end
-# 
+#
 #   def call
 #     input = ''
 #     while input != 'exit'
-# 
+#
 #       puts "Welcome to your music library!"
 #       puts "To list all of your songs, enter 'list songs'."
 #       puts "To list all of the artists in your library, enter 'list artists'."
@@ -125,7 +125,7 @@ end
 #       puts "To play a song, enter 'play song'."
 #       puts "To quit, type 'exit'."
 #       puts "What would you like to do?"
-# 
+#
 #       input = gets.strip
 #       case input.downcase
 #       when "list songs"
@@ -146,25 +146,25 @@ end
 #         call
 #     end
 #   end
-# 
+#
 #   def list_songs
 #     Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
 #       puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
 #     end
 #   end
-# 
+#
 #   def list_artists
 #     Artist.all.sort_by(&:name).each.with_index(1) do |art, idx|
 #       puts "#{idx}. #{art.name}"
 #     end
 #   end
-# 
+#
 #   def list_genres
 #     Genre.all.sort_by(&:name).each.with_index(1) do |gn, idx|
 #       puts "#{idx}. #{gn.name}"
 #     end
 #   end
-# 
+#
 #   def list_songs_by_artist
 #     puts "Please enter the name of an artist:"
 #       input = gets.strip
@@ -174,7 +174,7 @@ end
 #       end
 #     end
 #   end
-# 
+#
 #   def list_songs_by_genre
 #     puts "Please enter the name of a genre:"
 #       input = gets.strip
@@ -184,7 +184,7 @@ end
 #       end
 #     end
 #   end
-# 
+#
 #   def play_song
 #     puts "Which song number would you like to play?"
 #     song_num = gets.strip.to_i
@@ -195,5 +195,5 @@ end
 #       puts "Playing #{song.name} by #{song.artist.name}"
 #     end
 #   end
-# 
+#
 # end
