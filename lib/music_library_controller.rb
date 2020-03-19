c539955256cb6eb639d270dcb31e5924bc93c388
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-19 01:41:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -55,4 +55,16 @@ class MusicLibraryController
       end
     end
   end
+
+  def list_songs_by_genre
+    puts "Please enter the name of an artist:"
+      input = gets.strip
+      if genre = Genre.find_by_name(input)
+        genre.songs.sort_by(&:name).each.with_index(1) do |song, idx|
+          puts "#{idx}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+
+  
 end
